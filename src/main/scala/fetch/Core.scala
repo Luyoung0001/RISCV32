271d379638d7fad15c33239b1b40e92394ca7abb
luyoung0001 <luyoung0001@gmail.com>
2024-10-06 14:35:07 +0800

    finished chap7.test

--- src/main/scala/fetch/Core.scala
+++ src/main/scala/fetch/Core.scala
@@ -24,8 +24,26 @@ class Core extends Module {
 
     // 检测 inst, 如果是停机信号，那就停止
     io.exit := (inst === 0x34333231.U(WORD_LEN.W))
-    
+
+    // 解析寄存器地址
+    val rs1_addr = inst(19,15)
+    val rs2_addr = inst(24,20)
+    val wb_addr  = inst(11,7)
+
+    // 访问寄存器
+    val rs1_data = Mux(rs1_addr === 0.U(WORD_LEN.W), 0.U(WORD_LEN.W), regfile(rs1_addr))
+    val rs2_data = Mux(rs2_addr === 0.U(WORD_LEN.W), 0.U(WORD_LEN.W), regfile(rs2_addr))
+
     // 测试
+    // 打印 pc: inst
     printf(p"pc_reg: 0x${Hexadecimal(pc_reg)}--->inst: 0x${Hexadecimal(inst)}\n")
+    // 打印寄存器地址和值
+    printf(p"rs1_addr: 0x${Hexadecimal(rs1_addr)}--->rs1_data: 0x${Hexadecimal(rs1_data)}\n")
+    printf(p"rs2_addr: 0x${Hexadecimal(rs2_addr)}--->rs2_data: 0x${Hexadecimal(rs2_data)}\n")
+    printf(p"wb_addr: 0x${Hexadecimal(wb_addr)}\n")
+
+    // 一次测试分割线
+    printf("------------------------\n")
+
 
 }
